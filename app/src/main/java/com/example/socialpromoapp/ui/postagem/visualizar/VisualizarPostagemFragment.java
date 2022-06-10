69fe8199e92ce88d0868fc1b0880302f17ebd206
Nícolas Zanatto <34211411+NicolasZanatto@users.noreply.github.com>
2022-06-09 23:38:38 -0300

    Desenvolvimento edição

--- app/src/main/java/com/example/socialpromoapp/ui/postagem/visualizar/VisualizarPostagemFragment.java
+++ app/src/main/java/com/example/socialpromoapp/ui/postagem/visualizar/VisualizarPostagemFragment.java
@@ -8,6 +8,7 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -30,15 +31,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class VisualizarPostagemFragment extends Fragment implements OnMapReadyCallback {
     private FragmentVisualizarPostagemBinding binding;
     private GoogleMap mMap;
+    private String id;
     VisualizarPostagemViewModel cadastroViewModel;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
+        id = getArguments().getString("id");
         cadastroViewModel =
                 new ViewModelProvider(this).get(VisualizarPostagemViewModel.class);
-        cadastroViewModel.init(getArguments().getString("id"));
+        cadastroViewModel.init(id);
         binding = FragmentVisualizarPostagemBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         final TextView txtTitulo = binding.etTitulo;
@@ -46,6 +48,14 @@ public class VisualizarPostagemFragment extends Fragment implements OnMapReadyCa
         final TextView txtDescricao = binding.etDescricao;
         final TextView txtEstabelecimento = binding.etEstabelecimento;
         final TextView txtCategoria = binding.etCategoria;
+        final Button buttonEditar = binding.btnEditar;
+        buttonEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         cadastroViewModel.getPostagem().observe(getViewLifecycleOwner(), new Observer<PostagemModel>() {
             @Override
             public void onChanged(PostagemModel postagemModel) {
@@ -78,7 +88,7 @@ public class VisualizarPostagemFragment extends Fragment implements OnMapReadyCa
                             LatLng estabLatLng = new LatLng(lat, lng);
                             googleMap.addMarker(new MarkerOptions()
                                     .position(estabLatLng)
-                                    .title("Marker in Sydney"));
+                                    .title(estabelecimentoModel.getDescricao()));
                             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(estabLatLng,15));
                         }
                     }
